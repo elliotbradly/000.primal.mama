154d8de425dffe8ea6e3695bb9d7963f1faa410b
Brad Henderson <elliotbradly@gmail.com>
2025-06-21 08:34:50 -0400

    add progress

---
+++ 001.time/01.progress.unit/buz/progress.buzz.ts
@@ -0,0 +1,16 @@
+import { ProgressModel } from "../progress.model";
+import ProgressBit from "../fce/progress.bit";
+import State from "../../99.core/state";
+
+export const initProgress = (cpy: ProgressModel, bal: ProgressBit, ste: State) => {
+    debugger
+    return cpy;
+};
+
+export const updateProgress = (cpy: ProgressModel, bal: ProgressBit, ste: State) => {
+
+    debugger
+    return cpy;
+};
+
+
