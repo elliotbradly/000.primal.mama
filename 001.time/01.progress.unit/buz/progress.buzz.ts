ea50d8425ae0533ae393aaa7fe5c49a46bc4a574
Brad Henderson <elliotbradly@gmail.com>
2025-06-21 11:40:39 -0400

    update

--- 001.time/01.progress.unit/buz/progress.buzz.ts
+++ 001.time/01.progress.unit/buz/progress.buzz.ts
@@ -2,16 +2,33 @@ import { ProgressModel } from "../progress.model";
 import ProgressBit from "../fce/progress.bit";
 import State from "../../99.core/state";
 
+import * as ActClk from '../../03.clock.unit/clock.action'
+
+var bit, dat;
+
 export const initProgress = (cpy: ProgressModel, bal: ProgressBit, ste: State) => {
     debugger
     return cpy;
 };
 
-export const updateProgress = (cpy: ProgressModel, bal: ProgressBit, ste: State) => {
+export const updateProgress = async (cpy: ProgressModel, bal: ProgressBit, ste: State) => {
 
-    
+    bit = await ste.hunt(ActClk.READ_CLOCK, { idx: bal.idx })
+    var increment = bit.clkBit.dat;
 
-    debugger
+    var obj = { hours: 0, minutes: 0, days: 0, seconds: 0 }
+
+    //obj.hours = increment.hrs;
+    obj.minutes = increment.min;
+    obj.days = increment.day;
+    obj.seconds = increment.sec;
+
+    bit = await ste.hunt(ActClk.READ_CLOCK, { idx: bal.src })
+    dat = bit.clkBit.dat;
+
+    dat.bit.plus( obj );
+
+    bal.slv({ prgBit: { idx: 'update-progess', dat } });
     return cpy;
 };
 
