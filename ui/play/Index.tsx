d4702271eb2cb4dc3804a20b8dab66f98d2686d0
Brad Henderson <elliotbradly@gmail.com>
2025-07-08 09:30:51 -0400

    update

--- ui/play/Index.tsx
+++ ui/play/Index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { Box, Divider, ScrollArea } from '@mantine/core';
+import { Box, Divider, ScrollArea, Center, Button } from '@mantine/core';
 import { Title, Text, Stack, Container } from '@mantine/core';
 
 import Slider from './Slider'
@@ -8,7 +8,7 @@ import Slider from './Slider'
 import * as ActBab from '../../111.solid/01.babylon.unit/babylon.action'
 
 
-  
+
 export default function Component() {
 
     const [sliderValue, setSliderValue] = useState(40);
@@ -19,41 +19,59 @@ export default function Component() {
         console.log("Parent received new slider value:", newValue);
     };
 
+    var action = () => {
+
+
+    }
+
     setTimeout(() => {
         window['SOLID'](ActBab.OPEN_BABYLON, { src: 'surface00' })
     }, 333)
 
     return (
 
-        <Container size="md" p={0} style={{ backgroundColor: '#ab9d77', minHeight: '100vh', color: '#000' }}>
-            <Box p={40}>
+        <Center style={{ width: "100%", height: "100vh", padding: "0 20px" }}>
 
-                <Stack >
+            <Container size="md" p={0} style={{ backgroundColor: '#ab9d77', minHeight: '100vh', color: '#000' }}>
+                <Box p={40}>
 
-                    <Box style={{ display: 'flex', justifyContent: 'space-between', borderBottom: '1px solid #000' }}>
+                    <Stack >
 
-                        <Box style={{
-                            width: '960px',
-                            height: '540px',
-                            position: 'relative',
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                            overflow: "hidden"
-                        }}>
+                        <Box style={{ display: 'flex', justifyContent: 'space-between', borderBottom: '1px solid #000' }}>
 
-                            <canvas id='surface00' />
+                            <Box style={{
+                                width: '960px',
+                                height: '540px',
+                                position: 'relative',
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                overflow: "hidden"
+                            }}>
 
+                                <canvas id='surface00' />
+
+                            </Box>
                         </Box>
-                    </Box>
 
-                    <Slider value={sliderValue} onChange={handleSliderChange}/>
+                        <Slider value={sliderValue} onChange={handleSliderChange} />
 
-                  
 
-                </Stack>
+                        <Button onClick={action} size="lg" fullWidth>
+                            Record Description
+                        </Button>
 
-            </Box>
-        </Container>
+
+
+                    </Stack>
+
+                </Box>
+            </Container>
+
+
+
+        </Center>
+
+
     );
 
 
